21f2e590db17195a597bf2b851ca10c9b45c70be
John Bauer <johnbauer126@gmail.com>
2018-06-29 03:31:13 -0400

    switched to youtube

--- more-pluralsight/main.js
+++ more-pluralsight/main.js
@@ -1,24 +1,28 @@
 var w = 300
 var h = 100
 var padding = 2
-var dataset = [4, 8, 15, 16, 23, 42]
-var svg = d3.select('body').append('svg').attr('width', w).attr('height', h)
+var dataset = [5, 10, 14, 20, 25]
+var svg = d3.select('body').append('svg')
+            .attr('width', w)
+            .attr('height', h)
+
+function colorPicker(v) {
+    if (v<=20) {
+        return '#666666'
+    } else if (v>20) {
+        return '#f00033'
+    }
+}
 
 svg.selectAll('rect')
     .data(dataset)
     .enter()
     .append('rect')
-        .attr('x', function(d, i){
-            return i * (w/dataset.length)
-        })
-        .attr('y', function(d){
-            return h - (d*2)
-        })
-        .attr('width', w / dataset.length - padding)
-        .attr('height', function(d){
-            return d*2
-        })
-        .attr('fill', function(d) {
-            return 'rgb(' + '0,0,' + (d*10) + ')'
-        })
+.attr({
+    x: function(d, i) {return i * w / dataset.length},
+    y: function(d) {return h - d*4},
+    width: w / dataset.length - padding,
+    height: function(d) {return d*4},
+    fill: function(d) {return colorPicker(d)}
+})
 
